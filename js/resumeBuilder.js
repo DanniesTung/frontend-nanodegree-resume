0c2eae6485953e15844c1a798b58576d7add8b58
Dannies <s4smallfour@dongxiaosideMacBook-Pro.local>
2015-04-10 09:53:33 +0800

    Learning
    erase comment

---
+++ js/resumeBuilder.js
@@ -0,0 +1,29 @@
+var formattedName = HTMLheaderName.replace("%data%","Dannies");
+
+var role = "Web Developer";
+
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
+
+
+
+// var awesomeThoughts = "I am Dannies I am AWESOME!";
+// console.log(awesomeThoughts);
+// var funThoughts= 
+// 	awesomeThoughts.replace("AWESOME","Fun");
+// console.log(funThoughts);
+
+
+
+// var email = "s4smallfour@yahoo.com.tw";
+// var newEmail = 
+// 	email.replace( "yahoo.com.tw","gmail.com");
+
+// console.log(email);
+// console.log(newEmail);
+
